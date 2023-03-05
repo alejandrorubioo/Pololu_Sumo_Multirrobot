bc8c7a2d459fefebded196a129a9fe37a6197892
Luperbal <82396990+Luperbal@users.noreply.github.com>
2023-03-05 13:04:48 +0100

    Controladores motor ruedas
    He añadido al código del Pololo_Controller las líneas de código para acceder a los controladores y poder mover el robot

--- controllers/Pololo_Controller/Pololo_Controller.cpp
+++ controllers/Pololo_Controller/Pololo_Controller.cpp
@@ -8,7 +8,10 @@
 // <webots/DistanceSensor.hpp>, <webots/Motor.hpp>, etc.
 // and/or to add some other includes
 #include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
 
+#define TIME_STEP 64
+#define MAX_SPEED -6.28
 // All the webots classes are defined in the "webots" namespace
 using namespace webots;
 
@@ -24,17 +27,27 @@ int main(int argc, char **argv) {
   Robot *robot = new Robot();
 
   // get the time step of the current world.
-  int timeStep = (int)robot->getBasicTimeStep();
+  //int timeStep = (int)robot->getBasicTimeStep();
 
   // You should insert a getDevice-like function in order to get the
   // instance of a device of the robot. Something like:
   //  Motor *motor = robot->getMotor("motorname");
   //  DistanceSensor *ds = robot->getDistanceSensor("dsname");
   //  ds->enable(timeStep);
-
+  
+  Motor *leftMotor = robot->getMotor("Motor2");
+  Motor *rightMotor = robot->getMotor("Motor1");
+ 
+  leftMotor->setPosition(INFINITY);
+  rightMotor->setPosition(INFINITY);
+  
+  leftMotor->setVelocity(0.0);
+  rightMotor->setVelocity(0.0);
+  
   // Main loop:
   // - perform simulation steps until Webots is stopping the controller
-  while (robot->step(timeStep) != -1) {
+  //while (robot->step(timeStep) != -1) {
+  while (robot->step(TIME_STEP) != -1) {
     // Read the sensors:
     // Enter here functions to read sensor data, like:
     //  double val = ds->getValue();
@@ -43,6 +56,10 @@ int main(int argc, char **argv) {
 
     // Enter here functions to send actuator commands, like:
     //  motor->setPosition(10.0);
+    
+    leftMotor->setVelocity(MAX_SPEED);
+    rightMotor->setVelocity(MAX_SPEED/4);
+    
   };
 
   // Enter here exit cleanup code.
---
+++ controllers/Pololo_Controller/build/release/Pololo_Controller.d
@@ -0,0 +1,5 @@
+build/release/Pololo_Controller.o: Pololo_Controller.cpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/Robot.hpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/Motor.hpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/Device.hpp \
+  /Applications/Webots.app/Contents/include/controller/cpp/webots/../../c/webots/types.h
